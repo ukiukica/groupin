e7bceb40606b84f1a8d4de4779ce0c6982b37133
Caitlin Buen-Lucas <cbuenlucas@gmail.com>
2023-03-01 15:12:43 -0800

    spread nav links across page

---
+++ react-app/src/components/NavBar/index.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import LogoutButton from "../auth/LogoutButton";
+import "./NavBar.css";
+
+const NavBar = () => {
+  return (
+    <nav id="nav-links">
+      <div id="nav-left">
+        <NavLink
+          to="/"
+          exact={true}
+          activeClassName="active"
+          id="nav-home-link"
+        >
+          Home
+        </NavLink>
+      </div>
+      <div id="nav-right">
+        <NavLink
+          to="/login"
+          exact={true}
+          activeClassName="active"
+          className="nav-right-links"
+        >
+          Login
+        </NavLink>
+        <NavLink
+          to="/sign-up"
+          exact={true}
+          activeClassName="active"
+          className="nav-right-links"
+        >
+          Sign Up
+        </NavLink>
+        {/* <NavLink
+          to="/users"
+          exact={true}
+          activeClassName="active"
+          className="nav-right-links"
+        >
+          Users
+        </NavLink> */}
+        <LogoutButton />
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
