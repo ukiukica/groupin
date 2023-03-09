64ee692abb1018f63118c48e31b205cb0c6059bd
Caitlin Buen-Lucas <cbuenlucas@gmail.com>
2023-03-08 18:08:56 -0800

    changed login btn to read dashboard if session user

--- react-app/src/components/NavBar/index.js
+++ react-app/src/components/NavBar/index.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
 import LogoutButton from "../auth/LogoutButton";
 import "./NavBar.css";
 
 const NavBar = () => {
+  const sessionUser = useSelector(state => state.session.user);
+
+  console.log('sessionUser', sessionUser)
   return (
     <nav id="nav-links-div">
       <div id="nav-links">
@@ -18,6 +22,16 @@ const NavBar = () => {
           </NavLink>
         </div>
         <div id="nav-right">
+          {sessionUser ?
+          <NavLink
+            to="/dashboard"
+            exact={true}
+            activeClassName="active"
+            className="nav-right-links"
+          >
+            Dashboard
+          </NavLink>
+          :
           <NavLink
             to="/login"
             exact={true}
@@ -26,6 +40,7 @@ const NavBar = () => {
           >
             Login
           </NavLink>
+          }
           <NavLink
             to="/sign-up"
             exact={true}
