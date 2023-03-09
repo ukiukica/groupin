01a22b7edc53305b34ba5619e6df231224e3a818
Caitlin Buen-Lucas <cbuenlucas@gmail.com>
2023-03-08 17:48:14 -0800

    styled dashboard to reflect figma

--- react-app/src/components/Dashboard/index.js
+++ react-app/src/components/Dashboard/index.js
@@ -2,28 +2,30 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import GroupList from "../GroupList";
 import GroupDetails from "../GroupDetails";
-import './Dashboard.css'
+import "./Dashboard.css";
 
-function Dashboard () {
+function Dashboard() {
+  const userName = useSelector((state) => state.session.user.username);
 
-    const userName = useSelector((state) => state.session.user.username);
-    
-
-    return (
-        <>
-            <h1>Hello, {userName}!</h1>
-            <div>
-                <button>Pinned Groups</button>
-                <button>Suggested Groups</button>
-                <button>Browse All</button>
-                <div>Search...</div>
-                <GroupList />
-            </div>
-
-            <GroupDetails />
-
-        </>
-    )
+  return (
+    <div id="dashboard-container">
+      <div id="dashboard">
+        <div id="username-div">
+          <h1>Hello, {userName}!</h1>
+        </div>
+        <div id="dashboard-btns-div">
+          <button className="dashboard-btns">Pinned Groups</button>
+          <button className="dashboard-btns">Suggested Groups</button>
+          <button className="dashboard-btns">Browse All</button>
+        </div>
+        <br/>
+        <div>Search...</div>
+        <br/>
+        <GroupList />
+        <GroupDetails />
+      </div>
+    </div>
+  );
 }
 
 export default Dashboard;
--- react-app/src/components/Group/index.js
+++ react-app/src/components/Group/index.js
@@ -3,19 +3,19 @@ import { useDispatch, useSelector } from "react-redux";
 import "./Group.css";
 
 const Group = ({ group }) => {
-console.log('group', group)
+  console.log("group", group);
   return (
     <>
-      <p>{group.name}</p>
-      <div className="list-img-div">
-        <img
-          className="list-img"
-          src={group.img}
-          alt="img"
-        />
+    <div id="group-container">
+      <div className="group-img-div">
+        <img className="group-img" src={group.img} alt="img" />
       </div>
+      <div className="group-name-div">
+      <p className="group-name">{group.name}</p>
+      </div>
+    </div>
     </>
-  )
+  );
 };
 
 export default Group;
