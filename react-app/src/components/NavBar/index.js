32df55379c63dd988eee4f6fcfb947a385454ba8
Caitlin Buen-Lucas <cbuenlucas@gmail.com>
2023-03-08 18:03:25 -0800

    styled nav width to match width of dashboard

--- react-app/src/components/NavBar/index.js
+++ react-app/src/components/NavBar/index.js
@@ -5,35 +5,36 @@ import "./NavBar.css";
 
 const NavBar = () => {
   return (
-    <nav id="nav-links">
-      <div id="nav-left">
-        <NavLink
-          to="/"
-          exact={true}
-          activeClassName="active"
-          id="nav-home-link"
-        >
-          Home
-        </NavLink>
-      </div>
-      <div id="nav-right">
-        <NavLink
-          to="/login"
-          exact={true}
-          activeClassName="active"
-          className="nav-right-links"
-        >
-          Login
-        </NavLink>
-        <NavLink
-          to="/sign-up"
-          exact={true}
-          activeClassName="active"
-          className="nav-right-links"
-        >
-          Sign Up
-        </NavLink>
-        {/* <NavLink
+    <nav id="nav-links-div">
+      <div id="nav-links">
+        <div id="nav-left">
+          <NavLink
+            to="/"
+            exact={true}
+            activeClassName="active"
+            id="nav-home-link"
+          >
+            Home
+          </NavLink>
+        </div>
+        <div id="nav-right">
+          <NavLink
+            to="/login"
+            exact={true}
+            activeClassName="active"
+            className="nav-right-links"
+          >
+            Login
+          </NavLink>
+          <NavLink
+            to="/sign-up"
+            exact={true}
+            activeClassName="active"
+            className="nav-right-links"
+          >
+            Sign Up
+          </NavLink>
+          {/* <NavLink
           to="/users"
           exact={true}
           activeClassName="active"
@@ -41,7 +42,8 @@ const NavBar = () => {
         >
           Users
         </NavLink> */}
-        <LogoutButton />
+          <LogoutButton />
+        </div>
       </div>
     </nav>
   );
