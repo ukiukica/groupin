ef30d04522ef5ab8fb917b82fe564d330e02ead4
Uki Pavlovic <uki@ukis-air.mynetworksettings.com>
2023-03-08 19:33:04 -0500

    group details started

--- react-app/src/components/GroupDetails/index.js
+++ react-app/src/components/GroupDetails/index.js
@@ -1,8 +1,15 @@
 import React from "react";
 import "./GroupDetails.css";
 
-const GroupDetails = () => {
-  return <></>;
+const GroupDetails = ({ group }) => {
+  return (
+    <>
+      {/* <img className="list-img" src={group.img} alt="img" />
+      <h1>{group.name}</h1>
+      <p>{group.details}</p>
+      <a href={group.link}>Take me to the group</a> */}
+    </>
+  );
 };
 
 export default GroupDetails;
