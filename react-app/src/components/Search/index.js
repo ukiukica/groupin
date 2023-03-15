d6d225b5f73831076f990f9e9ff30ebcd98d0ec9
Uki Pavlovic <uki@ukis-air.mynetworksettings.com>
2023-03-15 11:51:56 -0400

    started search feature

---
+++ react-app/src/components/Search/index.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import "./Search.css";
+
+function Search({groupType}) {
+
+    const [query, setQuery] = useState("");
+    
+
+    return (
+        <>
+        </>
+    );
+};
+
+export default Search;
