d20ef77dc6c7b7fb226afb19c5e38f7a5c5ec5e4
Uki Pavlovic <ukiworks034@gmail.com>
2023-03-21 17:04:48 -0400

    logic from pin component added to dashboard component

--- react-app/src/components/Dashboard/index.js
+++ react-app/src/components/Dashboard/index.js
@@ -6,6 +6,10 @@ import "./Dashboard.css";
 
 function Dashboard() {
 
+  const sessionUserId = useSelector((state) => state.session.user.id);
+  const users = useSelector((state) => state.users);
+  const user = users[sessionUserId];
+
   const user = useSelector((state) => state.session.user);  // this should be changed to look like in Pin component
   const groupObjs = useSelector((state) => state.groups);
   const allGroups = Object.values(groupObjs);
